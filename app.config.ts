4944e0741a7e1694fe04f82591a3ccbf7be91ac9
Wicaksana Pratama <wicaksanapratama@gmail.com>
2023-11-30 14:35:36 +0700

    feat: Alter default config

--- app.config.ts
+++ app.config.ts
@@ -18,20 +18,20 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine', // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on', // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'wypratama',
+      instagram: 'wypratama',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs',
+        href: 'https://www.linkedin.com/in/wypratama',
       },
     },
     form: {
